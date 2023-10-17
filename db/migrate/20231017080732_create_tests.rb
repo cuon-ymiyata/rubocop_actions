920d065bfa69e634d774cded6e34e69a094daad5
cuon-ymiyata <ymiyata@cuon.co.jp>
2023-10-17 17:08:22 +0900

    TestModel

---
+++ db/migrate/20231017080732_create_tests.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateTests < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tests do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
